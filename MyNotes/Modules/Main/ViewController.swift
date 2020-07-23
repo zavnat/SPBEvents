1aeeea3f8b1abba3c9a5539a69e027172ad5c7fe
zavnat <safnat11@gmail.com>
2020-07-23 15:57:50 +0300

    change table view to collection view

--- MyNotes/Modules/Main/ViewController.swift
+++ MyNotes/Modules/Main/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UICollectionViewController {
   
   var presenter: ViewToPresenterProtocol?
   var configurator: MainConfiguratorProtocol = MainConfigurator()
