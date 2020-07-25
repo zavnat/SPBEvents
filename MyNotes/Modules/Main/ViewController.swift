f18c2078c3cbeb003bcf33837a7bddc39f7578a2
zavnat <safnat11@gmail.com>
2020-07-25 17:03:53 +0300

    customization code

--- MyNotes/Modules/Main/ViewController.swift
+++ MyNotes/Modules/Main/ViewController.swift
@@ -14,12 +14,10 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
   var presenter: ViewToPresenterProtocol?
   var configurator: MainConfiguratorProtocol = MainConfigurator()
   
-  var places = [Result]()
-  
-  
+  var places = [ViewModel]()
+ 
   override func viewDidLoad() {
     super.viewDidLoad()
-//    let width = view.frame.size.width / 2
     
     collectionView.delegate = self
     collectionView.dataSource = self
@@ -29,57 +27,48 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
     presenter?.startFetchingPlaces()
   }
   
-  @IBAction func addButtonPressed(_ sender: Any) {
-
-  }
-  
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return places.count
-    }
-    
+  }
+  
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PublishCollectionCell
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PublishCollectionCell
     cell.label.text = places[indexPath.row].title
-      let imageString = places[indexPath.row].images[0].image
-      let url = URL(string: imageString)
     cell.layer.cornerRadius = 10
-      cell.image.kf.setImage(with: url)
-      
-      return cell
-    }
-
- 
-   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    cell.image.kf.setImage(with: places[indexPath.row].image)
+    
+    return cell
+  }
+  
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     let height = collectionView.frame.height / 3
     
     let width  = collectionView.frame.width / 2 - 7
     return CGSize(width: width, height: height)
-
-      }
+  }
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
     return 1.5
   }
-
+  
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
     return 20.0
   }
-
-//        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-//            return 1.5
-//        }
-//
-//        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//            return 1.5
-//        }
+  override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    let offsetY = scrollView.contentOffset.y
+    let contentHeight = scrollView.contentSize.height
+    let frameHeight = scrollView.frame.height
+    presenter?.didScroll(offsrtY: offsetY, contentHeight: contentHeight, frameHeight: frameHeight)
+  }
+  
 }
 
 extension ViewController: PresenterToViewProtocol {
-  func showPlaces(placesArray: [Result]) {
-    self.places = placesArray
-    collectionView.reloadData()
-    
-
-    
+  func showPlaces(placesArray: [ViewModel]) {
+    self.places += placesArray
+    DispatchQueue.main.async {
+      self.collectionView.reloadData()
+    }
   }
   
   func showError() {
@@ -88,30 +77,4 @@ extension ViewController: PresenterToViewProtocol {
 }
 
 
-
-
-// MARK: - UITableViewDelegate, UITableViewDataSource
-//extension ViewController {
-//
-//  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//    return places.count
-//  }
-//
-//  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-//    cell.textLabel?.text = places[indexPath.row].title
-//
-//
-//    return cell
-//  }
-//
-//  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//    if let indexPath = tableView.indexPathForSelectedRow {
-//      let itemForDetail = places[indexPath.row]
-//      presenter?.router?.prepare(for: segue, data: itemForDetail)
-//    }
-//
-//  }
-//
-//}
 
