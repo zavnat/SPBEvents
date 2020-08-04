bfd3901c6a40fed41ae7a136b7fce897d5f3b455
zavnat <safnat11@gmail.com>
2020-08-04 17:30:24 +0300

    add new module

---
+++ MyNotes/Modules/Favorites/FavoritePresenter.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritePresenter.swift
+//  MyNotes
+//
+//  Created by admin on 04.08.2020.
+//  Copyright © 2020 Natali. All rights reserved.
+//
+
+import Foundation
---
+++ MyNotes/Modules/Favorites/FavoriteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteViewController.swift
+//  MyNotes
+//
+//  Created by admin on 04.08.2020.
+//  Copyright © 2020 Natali. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
