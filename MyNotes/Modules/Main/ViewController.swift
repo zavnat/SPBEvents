6cfebec49d11960caa77f734c2428cce492e9fc6
zavnat <safnat11@gmail.com>
2020-08-05 14:20:04 +0300

    refactoring code in view controller

--- MyNotes/Modules/Main/ViewController.swift
+++ MyNotes/Modules/Main/ViewController.swift
@@ -9,60 +9,62 @@
 import UIKit
 import Kingfisher
 
-
-class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, PublishCellDelegate {
- 
+class ViewController: UICollectionViewController {
+  
   var presenter: ViewToPresenterProtocol?
   var configurator: MainConfiguratorProtocol = MainConfigurator()
-  
   var places = [ViewModel]()
- 
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-   
     collectionView.delegate = self
     collectionView.dataSource = self
-    
-    print("view did load")
     configurator.configure(with: self)
     presenter?.startFetchingPlaces()
   }
   
+  //MARK: - CollectionViewDataSourse Methods
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return places.count
   }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PublishCollectionCell
+    
     cell.cellDelegate = self
     cell.label.text = places[indexPath.row].title
     cell.image.kf.setImage(with: places[indexPath.row].image)
-//    cell.inputAccessoryView?.tintColor = places[indexPath.row].favorite ? UIColor.red : .lightGray
     if places[indexPath.row].favorite {
       cell.favorites.setImage(UIImage(systemName: "heart.fill"), for: .normal)
-    }else {
+    } else {
       cell.favorites.setImage(UIImage(systemName: "heart"), for: .normal)
     }
-    
     return cell
   }
-
-
   
+  
+  //MARK: - CollectionViewDelegate Methods
   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    print(indexPath)
     presenter?.cellSelected(places [indexPath.row].id)
-    
   }
   
+  override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    let offsetY = scrollView.contentOffset.y
+    let contentHeight = scrollView.contentSize.height
+    let frameHeight = scrollView.frame.height
+    presenter?.didScroll(offsrtY: offsetY, contentHeight: contentHeight, frameHeight: frameHeight)
+  }
+}
+
+//MARK: - CollectionViewDelegateFlowLayout Methods
+extension ViewController: UICollectionViewDelegateFlowLayout {
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     let height = collectionView.frame.height / 3
-    
     let width  = collectionView.frame.width / 2 - 7
     return CGSize(width: width, height: height)
   }
+  
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
     return 1.5
   }
@@ -70,26 +72,9 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
     return 20.0
   }
-  override func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    let offsetY = scrollView.contentOffset.y
-    let contentHeight = scrollView.contentSize.height
-    let frameHeight = scrollView.frame.height
-    presenter?.didScroll(offsrtY: offsetY, contentHeight: contentHeight, frameHeight: frameHeight)
-  }
-  
-  func likePressed(cell: UICollectionViewCell) {
-    let indexPath = collectionView.indexPath(for: cell)
-    let item = places[indexPath!.row]
-    
-    let hasFavorite = item.favorite
-    places[indexPath!.row].favorite = !hasFavorite
-    collectionView.reloadItems(at: [indexPath!])
-    print(item.favorite)
-    print(item.title)
-   }
-
 }
 
+//MARK: - PresenterToViewProtocol Methods
 extension ViewController: PresenterToViewProtocol {
   func showPlaces(placesArray: [ViewModel]) {
     self.places = placesArray
@@ -100,6 +85,19 @@ extension ViewController: PresenterToViewProtocol {
   
   func showError() {
     print("Error fetching places")
+  }
+}
+
+//MARK: - CellDelegate Methods
+extension ViewController: PublishCellDelegate {
+  func likePressed(cell: UICollectionViewCell) {
+    guard let indexPath = collectionView.indexPath(for: cell) else {return}
+    let item = places[indexPath.row]
+    let hasFavorite = item.favorite
+    places[indexPath.row].favorite = !hasFavorite
+    collectionView.reloadItems(at: [indexPath])
+    print(item.favorite)
+    print(item.title)
   }
 }
 
